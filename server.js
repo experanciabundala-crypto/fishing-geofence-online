e83f4184a09ec4d8d0a0a9dfecb917d87293d349
experanciabundala-crypto <experanciabundala@gmail.com>
2026-07-19 20:24:00 +0300

    Update server.js

--- server.js
+++ server.js
@@ -89,9 +89,9 @@ const FORBIDDEN_ZONES = [
   {
     id: 'A',
     name: 'Kasekera - Eneo Lililokatazwa',
-    lat: -4.687981,
-    lon: 29.619278,
-    allowedError: 0.004492 // takriban mita 500
+    lat:  -4.651183,
+    lon:  29.440335,
+    allowedError: 0.044492 // takriban mita 500
   },
   {
     id: 'B',
